9efbb7154af3090b7dddb49120197c750c61505c
AvaWiebe01 <ava.wiebe04@gmail.com>
2025-11-18 15:59:24 -0500

    Additions

--- server-api/main.py
+++ server-api/main.py
@@ -1,17 +1,24 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class RoadData(BaseModel):
+    x_coord: float
+    y_coord: float
+    street_name: str
+    iri: float
 
 # Test endpoint
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/data/iri/{iri}/{x_coord}/{y_coord}")
-def post_road_data():
-    return None
+@app.post("/data")
+def post_road_data(data: RoadData):
+    return {"condition": "Data received!"}
 
 # Get conditions by coordinates
 @app.get("/conditions/coords/{x_coord}/{y_coord}")
