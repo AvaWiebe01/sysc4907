afc81fcb74fbb7cec9958ac829fb9b7c1832a996
JacksonFry <jacksonfry@cmail.carleton.ca>
2026-02-26 12:04:14 -0500

    Refactor shared_mem_for_iri.py for clarity and efficiency
    Removed unused imports and commented-out code. Updated the logic for processing readings and calculating IRI.

--- rpi-operation/shared_mem_for_iri.py
+++ rpi-operation/shared_mem_for_iri.py
@@ -1,9 +1,11 @@
-import time
 import numpy as np
-import posix_ipc
-import mmap
+import math 
+import scipy  
+import argparse
 import os
+import matplotlib.pyplot as plt
 import iriCalculator
+import numpy as np
 
 SHM_NAME = "shared_road_matrix"
 SEM_NAME = "access_matrix_sem"
@@ -117,15 +119,34 @@ class SharedResult:
 
 
 
+
 if __name__ == "__main__":
-    sharedIri = SharedResult() 
-    sharedMemRoadMatrix = SharedRoadMatrix()
+    #sharedIri = SharedResult() 
+    #sharedMemRoadMatrix = SharedRoadMatrix()
     
     
     while(True):
         readings = sharedMemRoadMatrix.read()
-        if (readings[150][0] == 1.0):
+       # j = 0
+        #readings = [[0 for _ in range(2)] for _ in range(151)]
+        
+        """while  j <= 149:
+            #readings[j][0] = j* 1.1000001
+
             
+            if (j % 2) == 0:
+                readings[j][1] = j*0.0003
+            else:
+                readings[j][1] = j*-0.0001
+            j+=1
+        """
+        #readings[150][0] = 1.0
+
+        if (readings[150][0] == 1.0):
+            segmentLength = readings[149][0]
+            result = iriCalculator.iri(np.array(readings[0:150]), segmentLength, readings[0][0], step=0, box_filter=False, method=2)
+            #print(result[0,2])
+            sharedIri.write(result[0,2])
             result = iriCalculator.iri(readings[0:149],0,0)
             sharedIri.write(result)
     
