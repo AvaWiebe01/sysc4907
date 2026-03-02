8da104244379dd2241c7d76d76836500e01afd7f
AvaWiebe01 <ava.wiebe04@gmail.com>
2026-03-02 16:46:45 -0500

    add GET data processing and return

--- server-api/api/main.py
+++ server-api/api/main.py
@@ -2,15 +2,19 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import Depends
+import pandas as pd
+import numpy as np
+from scipy import stats
 import pydantic
 import sqlmodel
 import requests
-import json
+from datetime import datetime
+import math
 
-### These constants must be the same as 
 IQ_SEARCH_RADIUS = 100
 UNNAMED_ROAD_STRING = "Unnamed"
 IQ_TOKEN = "pk.e27e659d87b04fd8f55014c2e2e82ccc"; # locationIQ API token
+OUTLIER_THRESHOLD = 3 # z_score threshold to drop outlier points
 
 # Format for receiving condition data from RPi
 # If this class is changed, the class "RoadData" in /rpi-api/apiRequests.py must also be updated.
@@ -50,23 +54,27 @@ def read_root():
 def post_road_data(data: RoadData = Depends()):
 
     # get the street name for this point from LocationIQ Nearest API - LONGITUDE BEFORE LATITUDE
-    url = (f"https://us1.locationiq.com/v1/nearest/driving/"
-           f"{data.lng:.7f},"
-           f"{data.lat:.7f}"
-           f"?radiuses={IQ_SEARCH_RADIUS}"
-           f"&key={IQ_TOKEN}"
-           f"&number=1")
-    print(url)
+
 
     # if we cannot find a nearby road, reject the data
     try:
+        url = (f"https://us1.locationiq.com/v1/nearest/driving/"
+            f"{data.lng:.7f},"
+            f"{data.lat:.7f}"
+            f"?radiuses={IQ_SEARCH_RADIUS}"
+            f"&key={IQ_TOKEN}"
+            f"&number=1")
+        print(url)
+
         resp = requests.get(url)
         resp_dict = resp.json()
         datapoint_streetname = resp_dict["waypoints"][0]["name"] if (resp_dict["waypoints"][0]["name"] != "") else UNNAMED_ROAD_STRING
+
         print("New point streetname:", datapoint_streetname)
+
     except:
         print("No nearby road found, data rejected")
-        return {"Response": "No nearby road found, data rejected"}
+        raise HTTPException(status_code=400, detail="No nearby road found, data rejected")
 
     # data point is validated by the pydantic model
     # Store received point in server's local database
@@ -107,31 +115,73 @@ def get_conditions_from_coordinates(lat: float, lng: float, radius: int = 200, s
     if (start > end):
         raise HTTPException(status_code=400, detail="start cannot be after end.")
 
-    # select specified data from RoadMonitor database
-    streetname = None
-    num_points = None
-    points_variance = None
-    roughness = None
+    # if start = end = 0, no range is specified and set "end" to current UNIX milliseconds
+    if (start == 0 and end == 0):
+        end = datetime.now().timestamp()*1000
 
-    # return the requested data to user
-        # lat: the requested latitude
-        # lng: the requested longitude
-        # radius: the requested search radius
-        # start: the requested time range start in UNIX epoch milliseconds
-        # end: the requested time range end in UNIX epoch milliseconds
-        # streetname: derived from locationIQ, roughness only pertains to data collected on this road
-        # num_points: the number of data points used in calculating roughness
-        # points_variance: the variance of the data points used in calculating roughness - higher = less reliable estimate
-        # roughness: the roughness of the street, determined from all data points within the radius and time range
-    return {
-        "lat": lat,
-        "lng": lng,
-        "radius": radius,
-        "start": start,
-        "end": end,
-        "streetname": streetname,
-        "num_points": num_points,
-        "points_variance": points_variance,
-        "roughness": roughness}
+    # get the closest road to the requested coordinates using locationIQ - LONGITUDE BEFORE LATITUDE
+    streetname = None
+    try: 
+        url = (f"https://us1.locationiq.com/v1/nearest/driving/"
+                f"{lng:.7f},"
+                f"{lat:.7f}"
+                f"?radiuses={IQ_SEARCH_RADIUS}"
+                f"&key={IQ_TOKEN}"
+                f"&number=1")
+        print(url)
+
+        resp = requests.get(url)
+        resp_dict = resp.json()
+        streetname = resp_dict["waypoints"][0]["name"] if (resp_dict["waypoints"][0]["name"] != "") else UNNAMED_ROAD_STRING
+
+        print("Request streetname:", streetname)
+
+     # if no road is close enough, raise an error
+    except:
+        print("No nearby road found, request rejected")
+        raise HTTPException(status_code=400, detail="No nearby road found. Please select coordinates which are closer to the road.")
+
+    # convert radius (m) to approximate longitude and latitude values
+    lat_radius = radius / 111_111 # conversion factor
+    lng_radius = radius / (111_111 * math.cos(lng*(math.pi/180))) # conversion factor using longitude in radians
+
+    # create the latitude/longitude boundaries, based on the requested radius and the formula [min + (value - min) % (max - min)] to wrap values
+    lat_bounds = [-90 + (lat-lat_radius+90) % 180, -90 + (lat+lat_radius+90) % 180]
+    lng_bounds = [-180 + (lng-lng_radius+180) % 360, -180 + (lng+lng_radius+180) % 360]
+    #TODO: Fix the SQL search if value wraps by doing two queries (will never happen in ottawa)
+
+    # select specified data from RoadMonitor database
+    with sqlmodel.Session(engine) as session:
+        statement = sqlmodel.select(DataPoint.roughness) \
+            .where(DataPoint.lat >= lat_bounds[0], DataPoint.lat <= lat_bounds[1]) \
+            .where(DataPoint.lng >= lng_bounds[0], DataPoint.lng <= lng_bounds[1]) \
+            .where(DataPoint.streetname == req_streetname) \
+            .where(DataPoint.timestamp >= start, DataPoint.timestamp <= end)
+            
+        matching_datapoints = session.exec(statement).all()
+        
+        # remove outliers from the data
+        df_datapoints = pd.DataFrame(matching_datapoints, columns=["Roughness"])
+        z_scores = np.abs(stats.zscore(df_datapoints["Roughness"]))
+        outlier_indices = np.where(z_scores > OUTLIER_THRESHOLD)[0]
+        
+        clean_data = df_datapoints.drop(outlier_indices)
+        num_points = clean_data.size # number of points used in the roughness calculation (excluding outliers)
+        print("Dropped " + len(outlier_indices)-num_points + "outliers out of " + df_datapoints.size + "data points")
+
+        points_variance = clean_data.var() # find variance of non-outlier values. Higher variance means we should be less confident
+        roughness = clean_data.mean() # average the non-outlier values
+
+        # return the requested data to user
+        return {
+            "lat": lat, # lat: the requested latitude
+            "lng": lng, # lng: the requested longitude
+            "radius": radius, # radius: the requested search radius
+            "start": start, # start: the requested time range start in UNIX epoch milliseconds
+            "end": end, # end: the requested time range end in UNIX epoch milliseconds
+            "streetname": streetname, # streetname: derived from locationIQ, roughness only pertains to data collected on this road
+            "num_points": num_points, # num_points: the number of data points used in calculating roughness
+            "points_variance": points_variance, # points_variance: the variance of the data points used in calculating roughness - higher = less reliable estimate
+            "roughness": roughness} # roughness: the roughness of the street, determined from all data points within the radius and time range
 
 
